251802905147d9f63d6c8d5622aa615a2dd278a3
wn1137162270 <1137162270@qq.com>
2019-11-12 14:46:10 +0800

    add MongoDB

--- build.sbt
+++ build.sbt
@@ -6,3 +6,5 @@ scalaVersion := "2.11.12"
 
 libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.4"
 libraryDependencies += "org.apache.spark" %% "spark-graphx" % "2.4.4"
+libraryDependencies += "org.mongodb.spark" %% "mongo-spark-connector" % "2.4.1"
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.4" % "compile"
--- src/main/scala/CollectNeighborIds.scala
+++ src/main/scala/CollectNeighborIds.scala
@@ -1,17 +1,23 @@
-import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.log4j.{Level, Logger}
-import org.apache.spark.graphx.{Edge, Graph, GraphLoader, VertexId, VertexRDD}
+import org.apache.spark.graphx.{Edge, Graph, VertexId, VertexRDD}
 import org.apache.spark.rdd.RDD
+import com.mongodb.spark._
+import org.apache.spark.sql.SparkSession
+import org.bson.Document
 
 object CollectNeighborIds {
 	def main(args: Array[String]): Unit = {
 		Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
 		Logger.getLogger("org.eclipse.jetty.server").setLevel(Level.OFF)
 
-		val conf=new SparkConf()
-				.setMaster("local[2]")
-				.setAppName("CollectNeighborIds")
-		val sc=new SparkContext(conf)
+		val sparkSession = SparkSession.builder()
+				.master("local[2]")
+				.appName("CollectNeighborIds")
+				.config("spark.mongodb.input.uri", "mongodb://127.0.0.1/db.GraphX")
+				.config("spark.mongodb.output.uri", "mongodb://127.0.0.1/db.rank")
+				.getOrCreate()
+
+		val sc=sparkSession.sparkContext
 
 		//构造图
 		val vertexNum = 629814
@@ -20,8 +26,8 @@ object CollectNeighborIds {
 			arrInt(a) = (a, 1)
 		}
 		val users: RDD[(VertexId, Int)] = sc.parallelize(arrInt)
-		val relationships: RDD[Edge[Int]] = sc.textFile("/Users/wn/Project/GraphX/data/output.txt")
-        		.map(line => Edge(line.split(" ")(0).toLong, line.split(" ")(1).toLong, 1))
+		val relationships: RDD[Edge[Int]] = MongoSpark.load(sparkSession).rdd
+				.map(row => Edge(row.getString(2).toLong, row.getString(1).toLong, 1))
 		val graph = Graph(users, relationships)
 		graph.cache()
 		val graphNeighborUtil=new GraphNeighborUtil
@@ -36,24 +42,23 @@ object CollectNeighborIds {
 
 		val subGraph=Graph(vertices,edges)
 
-		val firstNeighbor:VertexRDD[Double]=graph.pageRank(0.01).vertices
+		val firstNeighbor:VertexRDD[Double]=graph.pageRank(tol=0.01).vertices
 
-//		secondIds.foreach(println)
 		val neighborRank = firstNeighbor.filter(pred=>{
 			var flag=false
 			secondIds.foreach(id=>if(id == pred._1) flag = true)
 			flag
 		}).sortBy(x=>x._2,ascending=false)          //按照rank从大到小排序
-        		.map(t=>t._1+" "+t._2)
 				.coalesce(numPartitions=1)
-		neighborRank.foreach(println)
-		neighborRank.cache()
-		neighborRank.saveAsTextFile("/Users/wn/Project/GraphX/data/rank_"+id)
 
-		println("second neighbor rank:")
-		neighborRank.foreach(println)
-		println("top five rank:")
-		neighborRank.take(10).foreach(println)
+		val rankDocuments = neighborRank.map(e => Document.parse(s"{v: ${e._1}, r: ${e._2}}"))
+		MongoSpark.save(rankDocuments)
+
+		val rankString = neighborRank.map(t=>t._1+" "+t._2)
+		rankString.saveAsTextFile("/Users/wn/Project/GraphX/data/rank_"+id)
+
+		println("top ten rank:")
+		rankString.take(10).foreach(println)
 
 		sc.stop()
 
